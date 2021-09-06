7e7ee2f08c4482afa438f3a63dc4547f75ff828a
Amir <freedom4el@yandex.ru>
2021-09-06 23:27:31 +0500

    ДЗ 002-console

---
+++ 002-console/game-number.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const mystery = Math.floor(Math.random() * 101);
+
+console.log('Загадано число в диапазоне от 0 до 100 '+mystery);
+
+const input = readline.createInterface(process.stdin);
+
+input.on('line', (data) => {
+    if(data < mystery) {
+        console.log('Больше');
+    }
+    if (data > mystery) {
+        console.log('Меньше');
+    }
+    if (data == mystery) {
+        console.log('Отгадано число '+mystery);
+    }
+});
