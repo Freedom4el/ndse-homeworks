49368510c0973f73e897af00c3a8e3b47f7c3fd7
Amir <freedom4el@yandex.ru>
2021-10-12 19:34:51 +0500

    Подключение базы данных mongo

--- src/library/models/Book.js
+++ src/library/models/Book.js
@@ -1,16 +1,34 @@
-const { v4: uuid} = require('uuid');
+const {Schema, model} = require('mongoose');
 
-class Book {
-    constructor(id = uuid(), title = "", description = "", authors = "", favorite = "", fileCover = "", fileName = "", fileBook = "") {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.authors = authors;
-        this.favorite = favorite;
-        this.fileCover = fileCover;
-        this.fileName = fileName;
-        this.fileBook = fileBook;
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: "",
+    },
+    authors: {
+        type: String,
+        default: "",
+    },
+    favorite: {
+        type: String,
+        default: "",
+    },
+    fileCover: {
+        type: String,
+        default: "",
+    },
+    fileName: {
+        type: String,
+        default: "",
+    },
+    fileBook: {
+        type: String,
+        default: "",
     }
-}
+});
 
-module.exports = Book;
+module.exports = model('Book', bookSchema);
